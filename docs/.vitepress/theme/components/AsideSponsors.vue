f84e7420de9ca88a877ad3c82679f78bcb162892
hans <im.hansking@gmail.com>
2023-04-05 01:26:16 +0800

    Document Migration

---
+++ docs/.vitepress/theme/components/AsideSponsors.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { VPDocAsideSponsors } from 'vitepress/theme'
+
+const data = [
+  // {
+  //   items: [{ img: 'https://cdn.jsdelivr.net/gh/hansking98/picture/sponsor/wechat-color.jpg' }]
+  // },
+  // {
+  //   items: [{ img: 'https://cdn.jsdelivr.net/gh/hansking98/picture/sponsor/alipay-color.jpg' }]
+  // }
+]
+</script>
+
+<template>
+  <VPDocAsideSponsors :data="data" />
+</template>
+
+<style>
+.vp-sponsor-grid.medium .vp-sponsor-grid-image {
+  max-height: 90px;
+}
+</style>
---
+++ docs/fe/es6/code/nullish-coalescing.js
@@ -0,0 +1,11 @@
+/* ?? 运算符 */
+const a = 0 ?? 'hansking'         // 0
+const b = '' ?? 'hansking'        // ''
+const c = null ?? 'hansking'      // 'hansking'
+const d = undefined ?? 'hansking' // 'hansking'
+
+/* || 运算符 */
+const A = 0 || 'hansking'         // 'hansking'
+const B = '' || 'hansking'        // 'hansking'
+const C = null || 'hansking'      // 'hansking'
+const D = undefined || 'hansking' // 'hansking'
